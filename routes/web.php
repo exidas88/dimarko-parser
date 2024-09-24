e523df4ad1d5c12ea6abd80ccf8962c236f88f80
Boris Tetrev <tetrev@alterweb.sk>
2024-09-24 07:15:27 +0200

    add endpoint to run schedule

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/schedule-run', function () {
+    Artisan::call('schedule:run');
 });
